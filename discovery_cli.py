a9402113570fca31a8c31cf7ac0b47a80ff3cc3c
Technology At Darlowie-Security <technology@darlowie-security.com>
2026-01-25 20:14:45 +1100

    discover app

--- discovery_cli.py
+++ discovery_cli.py
@@ -5,9 +5,14 @@ import time
 import json
 from pathlib import Path
 
+from pydantic_ai.usage import RunUsage
+
+from json_schema_to_pydantic import create_model
+
 
 # local
 import darlowie
+from common import COLLECTION, RecordCollection
 from discovery_workflow import DiscoveryWorkflow
 
 
@@ -42,41 +47,78 @@ def testRun(discoveryWorkflow : DiscoveryWorkflow) -> list[str]:
 
     if "parseAttempt" in discoveryWorkflow.context and discoveryWorkflow.context["parseAttempt"]:
         startTime = time.time()
-        with open(discoveryWorkflow.context['rawtextfromPDF'], "r", encoding='utf8', errors='ignore') as txtIn:
-            textCombined = txtIn.read()
-        dictRawIssues, runUsage = discoveryWorkflow.parseAttemptRecordListOllama(textCombined)
+        if 'textCombined' not in locals():
+            # if this is a separate step - read text into string
+            with open(discoveryWorkflow.context['rawtextfromPDF'], "r", encoding='utf8', errors='ignore') as txtIn:
+                textCombined = txtIn.read()
+        listRawIssues, runUsage = discoveryWorkflow.parseAttemptRecordListOllama(textCombined)
         discoveryWorkflow.addUsage(runUsage)
         with open(discoveryWorkflow.context["rawJSON"], "w" , encoding="utf-8", errors="ignore") as rawJSONOut:
-            rawJSONOut.writelines(json.dumps(dictRawIssues, indent=2))
+            rawJSONOut.writelines(json.dumps(listRawIssues, indent=2))
         endTime = time.time()
         rawTextFromPDFBaseName = str(Path(discoveryWorkflow.context['rawtextfromPDF']).name)
-        msg = f"Discovered records in raw text {rawTextFromPDFBaseName}. Found {len(dictRawIssues)} potential issues. Time: {(endTime - startTime):9.2f} seconds"
+        msg = f"Discovered records in raw text {rawTextFromPDFBaseName}. Found {len(listRawIssues)} potential issues. Time: {(endTime - startTime):9.2f} seconds"
+        discoveryWorkflow.workerSnapshot(msg)
+
+    if "discoverSchema" in discoveryWorkflow.context and discoveryWorkflow.context["discoverSchema"]:
+        startTime = time.time()
+        if 'listRawIssues' not in locals():
+            # if this is a separate step - read raw JSON into list
+            with open(discoveryWorkflow.context['rawJSON'], "r", encoding='utf8', errors='ignore') as jsonIn:
+                listRawIssues = json.load(jsonIn)
+            msg = f"Read {len(listRawIssues)} raw records from file {discoveryWorkflow.context['inputFileBaseName']}."
+            discoveryWorkflow.workerSnapshot(msg)
+        schemaDict, runUsage = discoveryWorkflow.discoverRecordSchemaOllama(listRawIssues)
+        schemaDict = json.loads(schemaDict)
+        PydanticModel = create_model(schemaDict)
+        with open(discoveryWorkflow.context["schemaJSON"], "w", encoding='utf8', errors='ignore') as modelOut:
+            modelOut.writelines(json.dumps(schemaDict, indent=2))
+        endTime = time.time()
+        if runUsage:
+            discoveryWorkflow.addUsage(runUsage)
+            msg = f"discoverSchema: {PydanticModel != None}. Usage: {discoveryWorkflow.usageFormat(runUsage)}. Time: {(endTime - startTime):9.2f} seconds."
+        else:
+            msg = f"discoverSchema: {PydanticModel != None}. {(endTime - startTime):9.2f} seconds."
         discoveryWorkflow.workerSnapshot(msg)
 
     if "finalJSONfromRaw" in discoveryWorkflow.context and discoveryWorkflow.context["finalJSONfromRaw"]:
         startTime = time.time()
-        if 'dictRawIssues' not in locals():
-            # if this is a separate step - read raw JSON into record collection
+        if 'listRawIssues' not in locals():
+            # if this is a separate step - read raw JSON into list
             with open(discoveryWorkflow.context['rawJSON'], "r", encoding='utf8', errors='ignore') as jsonIn:
-                dictRawIssues = json.load(jsonIn)
-            msg = f"Read {len(dictRawIssues)} raw records from file {discoveryWorkflow.context['inputFileBaseName']}."
+                listRawIssues = json.load(jsonIn)
+            msg = f"Read {len(listRawIssues)} raw records from file {discoveryWorkflow.context['inputFileBaseName']}."
             discoveryWorkflow.workerSnapshot(msg)
 
-        for item in dictRawIssues:
-            print(item)
-            print("===============================")
-            oneRecord, runUsage = discoveryWorkflow.parseOneRecordOllama(item)
-            discoveryWorkflow.addUsage(runUsage)
-            aaa = json.loads(oneRecord)
-            print(aaa)
-            print("===============================")
-            print(json.dumps(aaa, indent=4))
-            print("===============================")
+        if 'PydanticModel' not in locals():
+            with open(discoveryWorkflow.context["schemaJSON"], "r", encoding='utf8', errors='ignore') as modelIn:
+                schemaDict = json.load(modelIn)
+            PydanticModel = create_model(schemaDict)
 
-            break
+        recordCollection = RecordCollection(
+            report = str(Path(discoveryWorkflow.context['inputFileName']).name),
+            finding_dict = {}
+        )
+        usageForPhase = RunUsage()
+
+        for item in listRawIssues:
+            startOneIssue = time.time()
+            oneRecord, runUsage = discoveryWorkflow.parseOneRecordOllama(item, PydanticModel)
+            key = oneRecord.identifier
+            recordCollection[key] = oneRecord
+            endOneIssue = time.time()
+            if runUsage:
+                discoveryWorkflow.addUsage(runUsage)
+                usageForPhase += runUsage
+                msg = f"Record: <b>{key}</b>. Usage: {discoveryWorkflow.usageFormat(runUsage)}. Time: {(endOneIssue - startOneIssue):9.2f} seconds."
+            else:
+                msg = f"Record: <b>{key}</b>. {(endOneIssue - startOneIssue):9.2f} seconds."
+            discoveryWorkflow.workerSnapshot(msg)
+
+        with open(discoveryWorkflow.context["finalJSON"], "w", encoding='utf8', errors='ignore') as jsonOut:
+            jsonOut.writelines(recordCollection.model_dump_json(indent=2))
 
         endTime = time.time()
-
         finalJSONBaseName = str(Path(discoveryWorkflow.context['finalJSON']).name)
         msg = f"Wrote final JSON: <b>{finalJSONBaseName}</b>. {(endTime - startTime):9.2f} seconds"
         discoveryWorkflow.workerSnapshot(msg)
@@ -102,6 +144,7 @@ def main():
     context["inputFileName"] = "webapp/indexer/input/AWS_Review.pdf"
     context["rawtextfromPDF"] = context["inputFileName"] + ".raw.txt"
     context["rawJSON"] = context["inputFileName"] + ".raw.json"
+    context["schemaJSON"] = context["inputFileName"] + ".schema.json"
     context["finalJSON"] = context["inputFileName"] + ".json"
     context["inputFileBaseName"] = str(Path(context["inputFileName"]).name)
 
@@ -110,6 +153,7 @@ def main():
 
     context["loadDocument"] = False
     context["parseAttempt"] = False
+    context["discoverSchema"] = False
     context["finalJSONfromRaw"] = True
 
     # text extraction from PDF
--- discovery_workflow.py
+++ discovery_workflow.py
@@ -1,7 +1,7 @@
 #
 # Discovery workflow class used by Django app and command line
 #
-from typing import List
+from typing import List, Dict
 from logging import Logger
 import json
 import re
@@ -77,38 +77,28 @@ class DiscoveryWorkflow(WorkflowBase):
             result = agent.run_sync(prompt)
             runUsage = result.usage()
             return result.output, runUsage
-        
+       
         except pydantic_ai.exceptions.UnexpectedModelBehavior:
             msg = "Exception: pydantic_ai.exceptions.UnexpectedModelBehavior"
             self.workerSnapshot(msg)
         except ValidationError as e:
             msg = f"Exception: ValidationError {e}"
             self.workerSnapshot(msg)
-
-        # attempt regexp match only if LLM match failed
-        return List(), RunUsage()
+        return None, None
 
 
-    def parseOneRecordOllama(self, docString : str) -> tuple[dict, RunUsage] :
-        """
-        Use Ollama host and Pydantic AI Agent 
-        
-        Args:
-            docs (str) - text with unstructured data
-
-        Returns:
-            LLM RunUsage
-        """
+    def discoverRecordSchemaOllama(self, docs : List) -> tuple[dict, RunUsage] :
 
         systemPrompt = f"""
         You are an expert in English text analysis.
-        The prompt contains English text that starts with identifier and contains fields including risk rating, status, description, recommendation.
-        Output JSON record where keys are names of fields.
-        Ensure the output is valid JSON as it will be parsed using `json.loads()` in Python.
-        Do not format output.
+        The prompt contains a list of repeated records. 
+        You must discover JSON schema of a single record. JSON schema must include all the fields that you find in any record.
+        Format output as JSON schema. 
+        Output only valid JSON, no Markdown or extra text.
+        Name identifier field 'identifier'
         """
         
-        prompt = f"{docString}"
+        prompt = f"{docs}"
 
         ollModel = self.createOpenAIChatModel()
 
@@ -127,4 +117,57 @@ class DiscoveryWorkflow(WorkflowBase):
             self.workerSnapshot(msg)
 
         # attempt regexp match only if LLM match failed
-        return {}, RunUsage()
+        return None, None
+
+
+
+    def parseOneRecordOllama(self, docString : str, Model: BaseModel) -> tuple[BaseModel, RunUsage] :
+        """
+        Use Ollama host and Pydantic AI Agent 
+        
+        Args:
+            docs (str) - text with unstructured data
+            Model (BaseModel) - optional Pydantic model
+
+        Returns:
+            LLM RunUsage
+        """
+
+        systemPrompt = f"""
+        You are an expert in English text analysis.
+        The prompt contains English text that starts with identifier and contains fields including risk rating, status, description, recommendation.
+        Output JSON record where keys are names of fields.
+        Name identifier field key 'identifier'
+        Ensure the output is valid JSON as it will be parsed using `json.loads()` in Python.
+        Do not format output.
+        """
+        
+        prompt = f"{docString}"
+
+        ollModel = self.createOpenAIChatModel()
+
+        if Model:
+            agent = Agent(ollModel,
+                        output_type=Model,
+                        system_prompt = systemPrompt)
+        else:
+            agent = Agent(ollModel,
+                        system_prompt = systemPrompt)
+        try:            
+            result = agent.run_sync(prompt)
+            if Model:
+                oneIssue = Model.model_validate_json(result.output.model_dump_json())
+            else:
+                oneIssue = result.output
+            runUsage = result.usage()
+            return oneIssue, runUsage
+        
+        except pydantic_ai.exceptions.UnexpectedModelBehavior:
+            msg = "Exception: pydantic_ai.exceptions.UnexpectedModelBehavior"
+            self.workerSnapshot(msg)
+        except ValidationError as e:
+            msg = f"Exception: ValidationError {e}"
+            self.workerSnapshot(msg)
+        return None, None
+    
+
